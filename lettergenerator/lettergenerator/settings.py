a9207aa3680d274f6a9b61ffd7d1333ae6932edc
Berke Şentürk <berkesenturk11@gmail.com>
2023-09-11 13:21:50 +0300

    debug setting falsed

--- lettergenerator/lettergenerator/settings.py
+++ lettergenerator/lettergenerator/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gtds5$zf^bjh0z&9ul))tlw5y%l)v7syb(#$b0lh_hgjog$3uz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
